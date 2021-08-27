5d7c1c00af8b38fec275f91147cd5cdd656b01a4
PHAM DUC THUAN <1721050507@student.humg.edu.vn>
2021-08-27 14:54:13 +0700

    bai2

---
+++ Bai2/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetInfo(string ten, double tuoi, double lop)
+        {
+            ViewBag.HovaTen = ten;
+            ViewBag.Tuoi = tuoi;
+            ViewBag.Lop = lop;
+            return View();
+        }
+    }
+}
